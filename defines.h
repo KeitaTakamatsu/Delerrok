8e724aed9e30558a4be4fa0dfb3eb7c03eea2c7b
Keita Takamatsu <keita.takamatsu2501@gmail.com>
2013-02-06 09:54:33 +0900

    changed header

--- defines.h
+++ defines.h
@@ -12,7 +12,10 @@
 #define SIZE_OF_AGENCY_ID 8
 #define SIZE_OF_ROUTE_ID 8
 #define SIZE_OF_STATION_ID 8
+#define SIZE_OF_ZONE_ID 8
+#define SIZE_OF_VALID_ZONE_LIST 10
 
+typedef unsigned long long u_int64;
 typedef unsigned int u_int32;
 typedef unsigned short u_int16;
 typedef unsigned char u_int8;
--- structures.h
+++ structures.h
@@ -14,7 +14,80 @@
 typedef struct
 {
     u_int8 appCode;
-}txn_t;
+    u_int8 agencyID[SIZE_OF_AGENCY_ID];
+    u_int8 routeID[SIZE_OF_ROUTE_ID];
+    u_int32 timestamp;
+    u_int64 cardID;
+    u_int8 cardType;
+    u_int16 expireDate;
+    int north;
+    int east;
+    u_int8 from[SIZE_OF_STATION_ID];
+    u_int8 to[SIZE_OF_STATION_ID];
+} __attribute__ ((packed)) txn_t;
+
+
+typedef struct
+{
+    u_int8 appCode;
+    u_int8 open;
+    short fare;
+    u_int8 messageCode;
+    u_int8 errorCode;
+    u_int8 sfpCode;
+    u_int16 passExpireDate;
+    short balance;
+}__attribute__ ((packed)) response_t;
+
+
+
+typedef struct
+{
+    u_int8 passType;
+    u_int32 passStartDate;
+    u_int32 passExpireDate;
+    u_int8 timeBasedPassAddTimeType;
+    u_int16 timeBasedPassAddTime;
+    u_int8 numOfTripBasedPass;
+    u_int8 validAgencyID[SIZE_OF_AGENCY_ID];
+    u_int8 validRouteID[SIZE_OF_ROUTE_ID];
+    u_int8 validZoneIDList[SIZE_OF_VALID_ZONE_LIST][SIZE_OF_ZONE_ID];
+}__attribute__ ((packed)) pass_t;
+
+
+typedef struct{
+    u_int8 zoneID[SIZE_OF_ZONE_ID];
+    u_int8 prevZoneID[SIZE_OF_ZONE_ID];
+    u_int8 nextZoneID[SIZE_OF_ZONE_ID];
+} __attribute__ ((packed)) zone_t;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #endif
